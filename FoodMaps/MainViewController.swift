6b483174775052f99ddf4cd25d5f721da833b577
hemg2 <hemg2@naver.com>
2023-10-09 10:58:09 +0900

    feat: storyboard 삭제

---
+++ FoodMaps/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  FoodMaps
+//
+//  Created by Hemg on 2023/10/09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
