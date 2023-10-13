754bd1b7f4c0dc7ee5f6bb03dcfa2c64dbeeecf7
hemg2 <hemg2@naver.com>
2023-10-13 11:30:23 +0900

    feat: Entity생성

---
+++ FoodMaps/Entity/DetailListCellData.swift
@@ -0,0 +1,13 @@
+//
+//  DetailListCellData.swift
+//  FoodMaps
+//
+//  Created by Hemg on 10/13/23.
+//
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ FoodMaps/Entity/LocationData.swift
@@ -0,0 +1,10 @@
+//
+//  LocationData.swift
+//  FoodMaps
+//
+//  Created by Hemg on 10/13/23.
+//
+
+struct LocationData: Decodable {
+    let documents: [Document]
+}
